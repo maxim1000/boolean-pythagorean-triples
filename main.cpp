8755fff5f5d5ad93a266a89ca91c7844b0869d30
maxim1000 <maxim1000@gmail.com>
2016-06-12 19:10:26 +0300

    cached triplets

--- main.cpp
+++ main.cpp
@@ -7,30 +7,57 @@ enum class TColor
 	White,
 	Undefined
 };
-bool IsCorrect(const std::vector<TColor> &colors)
+struct TTriplet
 {
-	for(int a=1;a<int(colors.size());++a)
+	int Elements[3];
+};
+std::vector<TTriplet> FindTriplets(int max)
+{
+	std::vector<TTriplet> triplets;
+	for(int a=1;a<=max;++a)
 	{
-		for(int b=a+1;b<int(colors.size());++b)
+		for(int b=a+1;b<=max;++b)
 		{
-			for(int c=b+1;c<int(colors.size());++c)
+			for(int c=b+1;c<=max;++c)
 			{
-				if(a*a+b*b==c*c && colors[a]==colors[b]&&colors[b]==colors[c])
-					return false;
+				if(a*a+b*b==c*c)
+					triplets.push_back({a,b,c});
 			}
 		}
 	}
+	return triplets;
+}
+bool IsCorrect(
+	const std::vector<TColor> &colors,
+	const std::vector<TTriplet> &triplets)
+{
+	for(const auto &triplet:triplets)
+	{
+		bool hasWhite=false;
+		bool hasBlack=false;
+		for(auto element:triplet.Elements)
+		{
+			if(colors[element]==TColor::Black)
+				hasBlack=true;
+			if(colors[element]==TColor::White)
+				hasWhite=true;
+		}
+		if(!hasBlack || !hasWhite)
+			return false;
+	}
 	return true;
 }
-bool IsPossible(std::vector<TColor> &colors)
+bool IsPossible(
+	std::vector<TColor> &colors,
+	const std::vector<TTriplet> &triplets)
 {
 	const auto undefined=std::find(colors.begin(),colors.end(),TColor::Undefined);
 	if(undefined==colors.end())
-		return IsCorrect(colors);
+		return IsCorrect(colors,triplets);
 	for(auto newColor:{TColor::Black,TColor::White})
 	{
 		*undefined=newColor;
-		if(IsPossible(colors))
+		if(IsPossible(colors,triplets))
 			return true;
 	}
 	*undefined=TColor::Undefined;
@@ -38,12 +65,13 @@ bool IsPossible(std::vector<TColor> &colors)
 }
 int main()
 {
-	for(int max=1;max<8000;++max)
+	for(int max=29;max<8000;++max)
 	{
 		const auto start=std::chrono::steady_clock::now();
 		std::cout<<max<<": ";
 		std::vector<TColor> colors(max+1,TColor::Undefined);
-		std::cout<<(IsPossible(colors)?"possible":"impossible");
+		const auto triplets=FindTriplets(max);
+		std::cout<<(IsPossible(colors,triplets)?"possible":"impossible");
 		const auto finish=std::chrono::steady_clock::now();
 		const auto ms=
 			std::chrono::duration_cast<std::chrono::milliseconds>(finish-start).count();
