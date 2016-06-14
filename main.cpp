b79abde7d81205291a370a6f8e9dd964e42c4c17
maxim1000 <maxim1000@gmail.com>
2016-06-14 22:28:00 +0300

    switched to the next number with processing time longer than 20s (4380)

--- main.cpp
+++ main.cpp
@@ -250,7 +250,7 @@ int main()
 {
 	try
 	{
-		for(int max=4100;max<8000;++max)
+		for(int max=4380;max<8000;++max)
 		{
 			const auto start=std::chrono::steady_clock::now();
 			std::cout<<max<<": ";
