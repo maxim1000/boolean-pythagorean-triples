50fddbe70ca03bd36cda393c6ed4b0f27bacdbb6
maxim1000 <maxim1000@gmail.com>
2016-06-24 09:26:58 +0300

    switched to the next number with time longer than 20s (4429)

--- main.cpp
+++ main.cpp
@@ -260,7 +260,7 @@ int main()
 {
 	try
 	{
-		for(int max=4360;max<8000;++max)
+		for(int max=4380;max<8000;++max)
 		{
 			const auto start=std::chrono::steady_clock::now();
 			std::cout<<max<<": ";
